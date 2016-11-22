c0b3a14c3117a7ee9f404d93c059914765c816fa
pagane <benedict@gbg.bg>
2016-11-22 13:09:19 +0200

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -2149,7 +2149,7 @@ function RunVoidMaps() {
         return;
     }
     if ((game.global.lastClearedCell > trimpzSettings["lastCell"].value && getRemainingTimeForBreeding()<1) || game.global.lastClearedCell > 96) {
-        if (ableToRunVoidMap(game.global.world+1) === false && ableToRunVoidMap(game.global.world-1) === true)
+        if (ableToRunVoidMap(game.global.world+1) === false && ableToRunVoidMap(game.global.world-2) === true && game.global.world%10<5 && game.global.world%10>0)
         {
             var theMap;
             for (var map in game.global.mapsOwnedArray) {
