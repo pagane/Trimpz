d1a7dee1d8cc668914ca29638eaca5e0c0261649
pagane <benedict@gbg.bg>
2016-11-22 17:06:58 +0200

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -1987,6 +1987,7 @@ function CheckPortal() {
     var map;
     var theMap;
     var itemsAvailable;
+    if (game.global.world%10>6 || game.global.world%10==0) return;
     if (game.global.world >= trimpzSettings["portalAt"].value - 2 && !game.global.portalActive && (game.resources.trimps.soldiers === 0 || game.resources.trimps.owned === game.resources.trimps.realMax()))
     {
         if (game.global.mapsActive)
