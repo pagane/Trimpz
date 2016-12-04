6e27b768bd2c2edf7e8a6164bc342f00202492c0
pagane <benedict@gbg.bg>
2016-12-04 12:22:58 +0200

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -1014,7 +1014,7 @@ function BuyEquipmentOrUpgrade(bestEquipGainPerMetal, bestUpgradeGainPerMetal, b
     else if (CanBuyNonUpgrade(game.equipment[bestEquipment], constants.getEquipmentCostRatio()) === true) {
         var upgrade = Object.keys(game.upgrades).filter(function(a){return game.upgrades[a].prestiges === bestEquipment;})[0];
         var upgradeStats = GetRatioForEquipmentUpgrade(upgrade, game.equipment[bestEquipment]);
-        if (upgradeStats.gainPerMetal < bestEquipGainPerMetal || (game.global.world>230 && game.equipment[bestEquipment].level<50)) {
+        if (upgradeStats.gainPerMetal < bestEquipGainPerMetal || (game.global.world>230 && game.equipment[bestEquipment].level<40) || game.global.world==trimpzSettings["voidMapsAt"].value) {
             buyEquipment(bestEquipment, true, true);
             return true;
         }
