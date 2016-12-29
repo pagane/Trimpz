b33967e85214f81ea5260eea65ccadccc952291b
pagane <benedict@gbg.bg>
2016-12-29 16:11:01 +0200

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -1833,10 +1833,19 @@ function RunMaps() {
     if (game.global.repeatMap){
         repeatClicked();
     }
+    
+    var skipCheck = false;
 
-//    if (game.global.lastBreedTime<30000 && game.resources.trimps.soldiers !== 0) return;
-//    if (getRemainingTimeForBreeding()>5) return;
-    if (game.global.lastClearedCell > 40 && game.global.world != trimpzSettings["voidMapsAt"].value)
+    if (game.global.challengeActive == "Daily")
+    {
+        if (typeof game.global.dailyChallenge.plague !== 'undefined' || typeof game.global.dailyChallenge.bogged !== 'undefined')
+            skipCheck = true;
+    }
+    else if (game.global.antiStacks<25 && game.global.lastClearedCell>50)
+        skipCheck = true;
+        
+    if (game.global.lastBreedTime<30000 && !skipCheck) return;
+    if (game.global.lastClearedCell > 50 && game.global.world != trimpzSettings["voidMapsAt"].value)
     {
         if (game.global.preMapsActive === true)
             RunWorld();
