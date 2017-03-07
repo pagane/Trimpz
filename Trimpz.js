e76575a02413543b57753561fa282dd4660c58d1
pagane <benedict@gbg.bg>
2017-03-07 16:06:59 +0200

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -563,6 +563,9 @@ function ClickAllNonEquipmentUpgrades() {
         if (upgrade === "Shieldblock"){
             continue;
         }
+        if (upgrade === "Coordination"){
+            continue;
+        }
         if (typeof game.upgrades[upgrade].prestiges == 'undefined' && game.upgrades[upgrade].locked === 0) {
             buyUpgrade(upgrade,true,true);  //Upgrade!
         }
@@ -694,6 +697,7 @@ function UpgradeNonEquipment() {
                 else
                     warpsAtLastGiga = game.buildings.Warpstation.owned;
             }
+            if (game.global.challengeActive == "Trapper" && upgrade == 'Coordination' && ableToOverkillAllMobs()) continue;
             if (upgrade == 'Coordination' && !canAffordCoordinationTrimps())
             {
                 if (unusedCoordsAt==0 && game.global.lastClearedCell>60)
