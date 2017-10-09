1c768ec2d28a017ec444af879edaf5c57a3f745f
pagane <benedict@gbg.bg>
2017-10-09 08:19:20 +0300

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -2232,7 +2232,7 @@ function RunVoidMaps() {
     }
 //    if (game.global.lastClearedCell > trimpzSettings["lastCell"].value && game.global.lastBreedTime>=45000 || game.global.lastClearedCell > 96) {
 //        if (ableToRunVoidMap(game.global.world+1) === false && ableToRunVoidMap(game.global.world-2) === true && game.global.world%10<5 && game.global.world%10>0 || (shouldPortal && portalAtWorld == game.global.world))
-    if (trimpzSettings["voidMapsAt"].value <= game.global.world &&/* ableToRunVoidMap(game.global.world+1) === false &&*/
+    if (trimpzSettings["voidMapsAt"].value == game.global.world &&/* ableToRunVoidMap(game.global.world+1) === false &&*/
         game.global.lastClearedCell > trimpzSettings["lastCell"].value && (game.global.lastBreedTime>=45000 || game.global.lastClearedCell > 96))
     {
         var theMap;
