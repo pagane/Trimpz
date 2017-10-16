24c42fac41fd9a47c2d3b48fe1f14aba5388c659
pagane <benedict@gbg.bg>
2017-10-16 15:03:54 +0300

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -1118,7 +1118,7 @@ function BuyMetalEquipment() {
     "use strict";
     
 //    if (getEnemyAttackForLevel(game.global.world)>game.global.soldierHealthMax/70)
-    if (game.global.soldierHealth/game.global.soldierHealthMax<trimpzSettings["hpEquipment"].value && getEmpowerment() != "Ice" && !(game.global.mapsActive === true && game.global.preMapsActive === false && getCurrentMapObject().location === "Void" && game.global.totalVoidMaps != 1))
+    if (game.global.soldierHealth/game.global.soldierHealthMax<trimpzSettings["hpEquipment"].value && getEmpowerment() != "Ice" && !(game.global.mapsActive === true && game.global.preMapsActive === false && getCurrentMapObject().location === "Void" && game.global.totalVoidMaps != 1) && !(ShouldStackWind() && game.global.antiStacks<45 && game.global.lastBreedTime>=45000))
         FindAndBuyEquipment("Health");
     FindAndBuyEquipment("Attack");
         
