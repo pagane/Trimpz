1f7502c36a7c45dad506bd5136e76db8cbb3b6c4
pagane <benedict@gbg.bg>
2017-11-30 15:13:44 +0200

    Add files via upload

--- Trimpz.js
+++ Trimpz.js
@@ -407,7 +407,7 @@ function AssignFreeWorkers() {
         {
             buyJob("Magmamancer", null, true);
         }
-        if (game.jobs.Explorer.locked === 0 && CanBuyWorkerWithResource(game.jobs.Explorer, constants.getExplorerCostRatio(), food, buy.Explorer)) !== -1)
+        if (game.jobs.Explorer.locked === 0 && CanBuyWorkerWithResource(game.jobs.Explorer, constants.getExplorerCostRatio(), food, buy.Explorer) !== -1)
         {
             buyJob("Explorer", null, true);
         }
@@ -1235,7 +1235,7 @@ function GotoMapsScreen() {
 //    }
 }
 
-function RunNewMap(zoneToCreate) {
+function RunNewMap(zoneToCreate, extra) {
     "use strict";
     var newMap;
     var size = 9;   //0-9
@@ -1248,6 +1248,23 @@ function RunNewMap(zoneToCreate) {
     if (game.global.challengeActive == "Metal")
         biome = "Mountain";
 
+    if (typeof zoneToCreate != 'undefined') {
+        document.getElementById("mapLevelInput").value = zoneToCreate;
+    }
+    
+    while (extra>0)
+    {
+        document.getElementById('advExtraLevelSelect').value = extra;
+        cost = updateMapCost(true);
+        if (cost < game.resources.fragments.owned) break;
+        extra--;
+    }
+    if (extra == 0)
+    {
+        RunWorld(); // no fragments
+        return;
+    }
+    
     difficultyAdvMapsRange.value = difficulty;
     adjustMap('difficulty', difficulty);
     sizeAdvMapsRange.value = size;
@@ -1255,10 +1272,6 @@ function RunNewMap(zoneToCreate) {
     lootAdvMapsRange.value = loot;
     adjustMap('loot', loot);
     biomeAdvMapsSelect.value = biome;
-    
-    if (typeof zoneToCreate != 'undefined') {
-        document.getElementById("mapLevelInput").value = zoneToCreate;
-    }
     
     var cost = updateMapCost(true);
 
@@ -1285,14 +1298,14 @@ function RunNewMap(zoneToCreate) {
     
     if (mapRunStatus === "Prestige")
     {
-        document.getElementById('advSpecialSelect').value = mapSpecialModifierConfig.p;
+        document.getElementById('advSpecialSelect').value = "p";
         cost = updateMapCost(true);
         if (cost > game.resources.fragments.owned)
             document.getElementById('advSpecialSelect').value = "0";
     }
     else
     {
-        document.getElementById('advSpecialSelect').value = mapSpecialModifierConfig.fa;
+        document.getElementById('advSpecialSelect').value = "fa";
         cost = updateMapCost(true);
         if (cost > game.resources.fragments.owned)
             document.getElementById('advSpecialSelect').value = "0";
@@ -1873,6 +1886,35 @@ function RunPrestigeMaps(){
     return false;
 }
 
+function RunFuturePrestigeMaps(){
+    "use strict";
+
+    if (!game.talents.blacksmith.purchased || game.global.challengeActive == "Mapology") return false;
+    if (game.global.world % 10 != 0 || getEmpowerment() != "Poison") return false;
+    
+    
+ 	var smithWorld = .5;
+	if (game.talents.blacksmith3.purchased) smithWorld = .9;
+	else if (game.talents.blacksmith2.purchased) smithWorld = 0.75;
+	smithWorld =  Math.floor((game.global.highestLevelCleared + 1) * smithWorld);
+	if (game.global.world <= smithWorld) return false;
+
+    var mapLevelToRun = game.global.world+5;
+    setMapRunStatus("Prestige");
+    for (var map in game.global.mapsOwnedArray){ //look for an existing map first
+        var theMap = game.global.mapsOwnedArray[map];
+        if (uniqueMaps.indexOf(theMap.name) > -1 || theMap.name.indexOf("Bionic Wonderland") > -1){
+            continue;
+        }
+        if (theMap.level === mapLevelToRun) {
+            RunMap(game.global.mapsOwnedArray[map]);
+            return true;
+        }
+    }
+    RunNewMap(game.global.world, 5);
+    return true;
+}
+
 /**
  * @return {boolean}
  */
@@ -1982,6 +2024,7 @@ function RunMaps() {
         recycleBelow(true);
 
     if (RunPrimaryUniqueMaps()) return;
+    if (RunFuturePrestigeMaps()) return;
     if (RunPrestigeMaps()) return;
     if (RunBetterMaps()) return;
 //    if (RunUpgradeMaps()) return;
@@ -2845,7 +2888,7 @@ function prettifyTime(timeSince)
 function ManageGenerator()
 {
     if (game.global.world<230 || !trimpzSettings["autoDG"].value) return;
-    if (game.global.world>trimpzSettings["voidMapsAt"].value-60 || game.global.world<400)
+    if (game.global.world>trimpzSettings["voidMapsAt"].value-50 || game.global.world<420)
         changeGeneratorState(0);
     else if (game.global.magmaFuel>game.generatorUpgrades.Capacity.modifier)
         changeGeneratorState(0);
